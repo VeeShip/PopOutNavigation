b2bd066f2c149146efdfca89d6acc0cf085d8af5
VeeShip <veeeship@gmail.com>
2015-02-23 18:17:55 -0500

    bug fixes for main.js; added readme

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 jQuery(document).ready(function($){
 	//open/close primary navigation
-	$('.cd-primary-nav-trigger').on('click', function(){
-		$('.cd-menu-icon').toggleClass('is-clicked'); 
+	$('.ts-nav-trigger').on('click', function(){
+		$('.ts-menu-icon').toggleClass('is-clicked'); 
 		
 		//in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
-		if( $('.cd-primary-nav').hasClass('is-visible') ) {
-			$('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+		if( $('.ts-nav').hasClass('is-visible') ) {
+			$('.ts-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
 				$('body').removeClass('overflow-hidden');
 			});
 		} else {
-			$('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+			$('.ts-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
 				$('body').addClass('overflow-hidden');
 			});	
 		}
