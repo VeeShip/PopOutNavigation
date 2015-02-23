2e6bfdfa412ae7873436986cdf7a7c95d4074712
VeeShip <veeeship@gmail.com>
2015-02-23 18:01:21 -0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+	//open/close primary navigation
+	$('.cd-primary-nav-trigger').on('click', function(){
+		$('.cd-menu-icon').toggleClass('is-clicked'); 
+		
+		//in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+		if( $('.cd-primary-nav').hasClass('is-visible') ) {
+			$('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+				$('body').removeClass('overflow-hidden');
+			});
+		} else {
+			$('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+				$('body').addClass('overflow-hidden');
+			});	
+		}
+	});
+});
