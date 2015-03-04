d08570a7d8c44e48083734ce5fee97d52729a0cf
VeeShip <veeeship@gmail.com>
2015-03-04 16:48:10 -0500

    removed .is-clicked class

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function($){
 	//Toggle NAV
 	$('.ts-nav-trigger').on('click', function(){
-		$('.ts-menu-icon').toggleClass('is-clicked'); 
 		if( $('.ts-nav').hasClass('is-visible') ) {
 			$('.nav-icon').removeClass('fa-remove').addClass('fa-bars');
 			$('.ts-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
