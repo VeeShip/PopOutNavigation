24499a6b464fcbfc8d320661b65254788af3d321
VeeShip <veeeship@gmail.com>
2015-03-04 20:29:45 -0500

    BugFixes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-jQuery(document).ready(function($){
+$(document).ready(function($){
 	//Toggle NAV
+
+	//MENU ICON : ON CLICK
 	$('.ts-nav-trigger').on('click', function(){
 		if( $('.ts-nav').hasClass('is-visible') ) {
 			$('.nav-icon').removeClass('fa-remove').addClass('fa-bars');
@@ -12,5 +14,21 @@ jQuery(document).ready(function($){
 				$('body').addClass('overflow-hidden');
 			});	
 		}
-	});
+	}), false;
+
+	// NAV LINKS : ON CLICK
+	$('.nav-btn').on('click', function() {
+			$('.nav-icon').removeClass('fa-remove').addClass('fa-bars');
+			$('.ts-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+				$('body').removeClass('overflow-hidden');
+			});
+	}), false;
+
+	// LOGO : ON CLICK
+	$('.ts-logo').on('click', function() {
+			$('.nav-icon').removeClass('fa-remove').addClass('fa-bars');
+			$('.ts-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+				$('body').removeClass('overflow-hidden');
+			});
+	}), false;
 });
